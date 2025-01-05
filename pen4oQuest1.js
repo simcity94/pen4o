2ffb6bb991857b01d012184f31487f3d5069793d
simcity94 <simo_ii94@abv.bg>
2025-01-05 21:23:01 +0200

    Add files via upload

---
+++ pen4oQuest1.js
@@ -0,0 +1,13 @@
+function pen4oQuest1(sideA, sideB, sideC){
+    if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB)
+
+console.log(`The Triangle of Truth with sides ${sideA}, ${sideB}, and ${sideC} is invalid. Pen4o remains stuck!`);
+
+  else 
+
+
+console.log(`The Triangle of Truth with sides ${sideA}, ${sideB}, and ${sideC} has been validated. Pen4o, you may begin your journey!`);
+console.log("Great success!")
+ }
+
+pen4oQuest1(3, 4, 5)
