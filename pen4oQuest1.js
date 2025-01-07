4c067a3d5502f1cbb6d417000d89517dceaeece9
simcity94 <simo_ii94@abv.bg>
2025-01-07 17:09:01 +0200

    Update pen4oQuest1.js

--- pen4oQuest1.js
+++ pen4oQuest1.js
@@ -1,13 +1,10 @@
 function pen4oQuest1(sideA, sideB, sideC){
-    if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB)
-
-console.log(`The Triangle of Truth with sides ${sideA}, ${sideB}, and ${sideC} is invalid. Pen4o remains stuck!`);
-
-  else 
-
-
-console.log(`The Triangle of Truth with sides ${sideA}, ${sideB}, and ${sideC} has been validated. Pen4o, you may begin your journey!`);
-    
- }
-
-pen4oQuest1(3, 4, 5)
+  if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB) {
+    console.log(`The Triangle of Truth with sides ${sideA}, ${sideB}, and ${sideC} has been validated. Pen4o, you may begin your journey!`);
+      console.log("Great Success");
+  } else {
+    console.log(`The Triangle of Truth with sides ${sideA}, ${sideB}, and${sideC} is invalid. Pen4o remains stuck!`);
+      console.log("Fail!");
+  }
+}
+pen4oQuest1(1, 2, 3)
